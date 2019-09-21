66a07261d916eda08ef6208159b38c950c251841
deano86 <53668176+deano86@users.noreply.github.com>
2019-09-21 00:54:22 -0400

    Update web.js

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ function initHome() {
 }
 
 function initImg() {
-	var img1 = 'dd1_2_s.jpg';
+	var img1 = 'dd_new.jpg';
 	var img2 = 'jump.jpg';
 	var img3 = 'panda1.jpg';
 	var element = $('#home-img');
